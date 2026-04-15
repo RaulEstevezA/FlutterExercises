1d9d7e97e295a8fe4d16d2c1073a363e6470a342
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-04-15 14:20:12 +0200

    cambiada la forma de obtener la informacion de json para un mapeo mejor

--- flutter_first_steps/03_maybe_app/lib/config/helpers/get_yes_no_answer.dart
+++ flutter_first_steps/03_maybe_app/lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:maybe_app/domain/entities/message.dart';
 import 'package:maybe_app/infrastructure/models/yes_no_model.dart';
@@ -10,17 +8,8 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
-    final data = response.data;
-    final jsonMap = data is String
-        ? Map<String, dynamic>.from(jsonDecode(data))
-        : Map<String, dynamic>.from(data);
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    final yesNoModel = YesNoModel.fromJsonMap(jsonMap);
-
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.hers,
-      imageUrl: yesNoModel.image,
-    );
+    return yesNoModel.toMessageEntity();
   }
 }
--- flutter_first_steps/03_maybe_app/lib/infrastructure/models/yes_no_model.dart
+++ flutter_first_steps/03_maybe_app/lib/infrastructure/models/yes_no_model.dart
@@ -1,17 +1,32 @@
+import 'package:maybe_app/domain/entities/message.dart';
+
 class YesNoModel {
-  final String answer;
-  final bool forced;
-  final String image;
+    final String answer;
+    final bool forced;
+    final String image;
 
-  YesNoModel({
-    required this.answer,
-    required this.forced,
-    required this.image,
-  });
+    YesNoModel({
+        required this.answer,
+        required this.forced,
+        required this.image,
+    });
 
-  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
-        answer: json['answer'] as String,
-        forced: (json['forced'] ?? json['focer'] ?? false) as bool,
-        image: json['image'] as String,
-      );
+    factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+    };
+
+    Message toMessageEntity() => Message(
+      text: answer == 'yes' ? 'Si' : 'No',
+      fromWho: FromWho.hers,
+      imageUrl: image,
+    );
 }
+
