ae49de710c0bd33929c1b00b0877e05da743a36f
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-04-22 17:21:07 +0200

    creado widget de videos verticales

--- flutter_first_steps/04_tok_tik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ flutter_first_steps/04_tok_tik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -10,6 +10,8 @@ class VideoScrollableView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
       children: [
         Container( color: Colors.red),
         Container( color: Colors.blue)
