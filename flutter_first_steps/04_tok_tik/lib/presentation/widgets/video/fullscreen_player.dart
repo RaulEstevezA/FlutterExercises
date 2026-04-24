72a927bec734ff11f819921a3f277a9517e01a06
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-04-24 14:22:35 +0200

    Añadidos controles tactiles de pausa/play en el video

--- flutter_first_steps/04_tok_tik/lib/presentation/widgets/video/fullscreen_player.dart
+++ flutter_first_steps/04_tok_tik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -48,22 +48,34 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2));
         }
 
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
+        return GestureDetector(
 
-              // Gradiente
+          onTap: (){
+            if ( controller.value.isPlaying ){
+              controller.pause();
+              return;
+            }
 
-              //Texto
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption)
-                ),
+            controller.play();
+          },
 
-            ],
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+          
+                // Gradiente
+          
+                //Texto
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption)
+                  ),
+          
+              ],
+            ),
           ),
         );
       },
