6eb3d2f1c7633619d3f36a3e96a33e9f5dfcafd3
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-04-29 13:48:42 +0200

    modificado el provider para que use los archivos intermedios de obtención de videos

--- flutter_first_steps/04_tok_tik/lib/presentation/provider/discover_provider.dart
+++ flutter_first_steps/04_tok_tik/lib/presentation/provider/discover_provider.dart
@@ -1,23 +1,27 @@
 
 import 'package:flutter/material.dart';
 import 'package:tok_tik/domain/entitites/video_post.dart';
-import 'package:tok_tik/infrastructure/models/local_video_model.dart';
-import 'package:tok_tik/shared/data/local_video_post.dart';
+import 'package:tok_tik/domain/repositories/video_post_repository.dart';
+
 
 class DiscoverProvider extends ChangeNotifier {
 
-  // TODO: Repository, DataSource
+  final VideoPostRepository videosRepository;
   
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({required this.videosRepository});
 
   Future<void> loadNextPage() async{
 
     //await Future.delayed( const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts.map( 
-      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
-      ).toList();
+    // final List<VideoPost> newVideos = videoPosts.map( 
+    //   (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+    //   ).toList();
+
+    final newVideos = await videosRepository.getTrendingvideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
