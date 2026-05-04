ef5d2da5179b971364c4cb80558897f7551bbc6e
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-04 13:31:13 +0200

    Implementado el uso de rutas con nombre

--- flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
+++ flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
@@ -6,21 +6,25 @@ import 'package:widget_app/presentation/screens/screens.dart';
 
 
 final appRouter = GoRouter(
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: "/",
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
 
     ),
 
     GoRoute(
       path: "/buttons",
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
 
     ),
 
     GoRoute(
       path: "/cards",
+      name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
 
     ),
