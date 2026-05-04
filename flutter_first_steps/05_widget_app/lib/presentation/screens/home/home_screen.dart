a453fd2e3f99fcc750cc2aa1e5a5c94e3704cee2
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-04 12:55:35 +0200

    Creacion de enlace a las ventanas en la App

--- flutter_first_steps/05_widget_app/lib/presentation/screens/home/home_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
+import 'package:widget_app/presentation/screens/bottons/buttons_screen.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -51,6 +53,19 @@ class _CustomListTitle extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
+      onTap: (){
+
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   )
+        // );
+
+        Navigator.pushNamed(context, menuItem.link);
+
+
+      },
       );
+      
   }
 }
