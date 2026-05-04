eed62f2923091117ac3c8b3dccc2bb564d192946
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-04 12:55:17 +0200

    Modificacion de nombre de ruta cards

--- flutter_first_steps/05_widget_app/lib/config/menu/menu_items.dart
+++ flutter_first_steps/05_widget_app/lib/config/menu/menu_items.dart
@@ -31,7 +31,7 @@ const appMenuItems = <MenuItems>[
   MenuItems(
     title: 'Tarjetas',
     subTitle: 'Un contenedor estilizado',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card,
   ),
 
