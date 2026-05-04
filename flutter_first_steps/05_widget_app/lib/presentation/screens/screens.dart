8dd8da38083dab95dfaaef99d3d0d15244de533a
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-04 13:20:08 +0200

    Creacion de importaciones

---
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+
+export 'package:widget_app/presentation/screens/bottons/buttons_screen.dart';
+export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
+export 'package:widget_app/presentation/screens/home/home_screen.dart';
+
