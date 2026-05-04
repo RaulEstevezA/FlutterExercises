ec28a85d010e9455d758f80c8318e9390e5e04b5
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-04 12:55:55 +0200

    Añadida funcionalidad de creación de ventanas por rutas

--- flutter_first_steps/05_widget_app/lib/main.dart
+++ flutter_first_steps/05_widget_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widget_app/config/theme/app_theme.dart';
+import 'package:widget_app/presentation/screens/bottons/buttons_screen.dart';
 import 'package:widget_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -15,6 +16,10 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
       home: const HomeScreen(),
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const ButtonsScreen(),
+      },
     );
   }
 }
