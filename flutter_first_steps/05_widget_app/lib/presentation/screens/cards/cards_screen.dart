8a8d372c297be4f90ac12d3350f2c36b82b3ed62
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-05 10:59:58 +0200

    tarjeta tipo 4 implantada

--- flutter_first_steps/05_widget_app/lib/presentation/screens/cards/cards_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/cards/cards_screen.dart
@@ -47,6 +47,10 @@ class _CardsView extends StatelessWidget {
             (card) => _CardType3(elevation: card['elevation'], label: card['label'],)
           ),
 
+          ...cards.map(
+            (card) => _CardType4(elevation: card['elevation'], label: card['label'],)
+          ),
+
 
           const SizedBox(height: 50),
 
@@ -195,28 +199,34 @@ class _CardType4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.hardEdge,
       elevation: elevation,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
-        child: Stack(
-          children: [
-            Image.network(
-              'https://picsum.photos/id/${ elevation.toInt() }/600/250'
-            ),
-
-            Align(
-              alignment: Alignment.topRight,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${ elevation.toInt() }/600/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+      
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only( bottomLeft: Radius.circular(20)),
+              ),
               child: IconButton(
                 onPressed: (){}, 
                 icon: const Icon(Icons.more_vert_outlined),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Text('$label - image'),
-            )
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - image'),
+          )
+        ],
       ),
     );
   }
