0c6b01b02185ec9b9a875e76fc2d9f73ecb5b943
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-08 14:25:51 +0200

    creado contenedor circular azul en animated screeen

--- flutter_first_steps/05_widget_app/lib/presentation/screens/animated/animated_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/animated/animated_screen.dart
@@ -13,6 +13,19 @@ class AnimatedScreen extends StatelessWidget {
         title: Text('Animated Container'),
       ),
 
+
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: const Icon(Icons.play_arrow_rounded)
