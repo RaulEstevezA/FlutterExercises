2051325fd2a911d9986f5b2e82fcafd5c1996a79
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-08 14:55:38 +0200

    eliminado importe innecesario

--- flutter_first_steps/05_widget_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:widget_app/config/theme/app_theme.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
