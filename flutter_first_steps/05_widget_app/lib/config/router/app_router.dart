f51516aa21fb3c2b71498b1d071d25757d8beb82
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-08 14:56:38 +0200

    Creada ruta de UI

--- flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
+++ flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
@@ -49,5 +49,12 @@ final appRouter = GoRouter(
       builder: (context, state) => const AnimatedScreen(),
 
     ),
+
+    GoRoute(
+      path: "/ui-controls",
+      name: UiControlsScreen.name,
+      builder: (context, state) => const UiControlsScreen(),
+
+    ),
   ],
 );
