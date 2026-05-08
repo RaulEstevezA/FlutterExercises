c29123be21969fd621b72aecda6e12ea6a25f2c4
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-08 14:20:39 +0200

    Creada la ruta

--- flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
+++ flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
@@ -42,5 +42,12 @@ final appRouter = GoRouter(
       builder: (context, state) => const SnackbarScreen(),
 
     ),
+
+    GoRoute(
+      path: "/animated",
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+
+    ),
   ],
 );
