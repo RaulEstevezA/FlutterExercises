1c33a562eeda265bb55adbfbb61ec64d097b79bf
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-11 11:01:14 +0200

    Creada ruta para la ventana Tutorial

--- flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
+++ flutter_first_steps/05_widget_app/lib/config/router/app_router.dart
@@ -56,5 +56,12 @@ final appRouter = GoRouter(
       builder: (context, state) => const UiControlsScreen(),
 
     ),
+
+    GoRoute(
+      path: "/tutorial",
+      name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen(),
+
+    ),
   ],
 );
