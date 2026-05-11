64273a51ed92ca03df5a6da560e64f47ba7694b0
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-11 11:15:17 +0200

    creada clase SlideInfo y constructores

--- flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo<[
+  
+]
+
+
+
 class AppTutorialScreen extends StatelessWidget {
 
   static const name = 'tutorial_screen';
@@ -8,6 +22,15 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          
+        ],
+      ),
+
+    );
   }
 }
