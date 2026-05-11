a63bbae971d8ad872d55b99e7f12facc21c944e7
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-11 11:18:21 +0200

    Creados los Slides

--- flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -8,9 +8,11 @@ class SlideInfo {
   SlideInfo(this.title, this.caption, this.imageUrl);
 }
 
-final slides = <SlideInfo<[
-  
-]
+final slides = <SlideInfo>[
+  SlideInfo('Busca la comida', 'Laboris esse dolore nostrud ex Lorem deserunt eu. Dolor elit aliqua dolor velit ea est velit sit culpa ipsum quis aliqua. Nisi labore mollit fugiat ut dolor et veniam velit laborum proident deserunt ad ex incididunt. Quis nostrud ullamco consectetur enim do ad fugiat cillum ullamco culpa qui ipsum veniam. Sint laborum cillum veniam occaecat aliqua sit pariatur id laborum. Sunt esse ipsum elit reprehenderit consequat id cillum. Tempor fugiat cupidatat est nisi et enim cupidatat qui.', 'assets/images/1.png'),
+  SlideInfo('Entrega rápida', 'Consectetur voluptate eiusmod aliqua cillum ullamco ipsum ut mollit velit. Eu esse nulla ad exercitation. Id irure aliqua aliquip Lorem irure esse dolore dolore fugiat. Anim exercitation ullamco aliqua adipisicing aliqua incididunt id elit id sunt aliqua magna aute. Laborum esse laborum dolor voluptate incididunt do magna deserunt elit.', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Consequat minim excepteur eiusmod magna incididunt pariatur culpa laboris est veniam consequat sunt est. Cillum minim anim laboris proident dolore in occaecat irure voluptate quis aute incididunt voluptate. Cillum duis amet cupidatat ex nulla anim aute ullamco amet. Aute ipsum enim sit adipisicing qui aliqua enim ipsum eiusmod.', 'assets/images/3.png'),
+];
 
 
 
