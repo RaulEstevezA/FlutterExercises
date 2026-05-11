40f8a6052b6aaad49da9b79b74411ce772231640
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-11 13:49:55 +0200

    Enlaces activos

--- flutter_first_steps/05_widget_app/lib/presentation/widgets/side_menu.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
@@ -20,9 +21,15 @@ class _SideMenuState extends State<SideMenu> {
     return NavigationDrawer(
       selectedIndex: navDraweIndex,
       onDestinationSelected: (valeu){
+
         setState(() {
           navDraweIndex = valeu;
         });
+
+        final menuItem = appMenuItems[valeu];
+
+        context.push(menuItem.link);
+
       },
       children: [
         Padding(
