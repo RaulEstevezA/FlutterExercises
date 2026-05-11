58ee48ec0fb1cd43fa005d9ee474f3cd06c95dfc
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-11 11:43:10 +0200

    Modificados estilo  de los textos

--- flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class SlideInfo {
@@ -51,7 +53,8 @@ class _Slide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-
+        final titleStyle = Theme.of(context).textTheme.titleLarge;
+        final captionStyle = Theme.of(context).textTheme.bodySmall;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
@@ -62,9 +65,9 @@ class _Slide extends StatelessWidget {
           children: [
             Image(image: AssetImage(imageUrl)),
             const SizedBox(height: 20), 
-            Text(title),
+            Text(title, style: titleStyle,),
             const SizedBox(height: 10),
-            Text(caption),
+            Text(caption, style: captionStyle,),
           ],
         ),
       ),
