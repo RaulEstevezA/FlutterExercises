b6f2a09b4ca6601afd88dc47488594e31849257f
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 12:40:59 +0200

    Modificado de sitio el archivo junto a modificado el nombre por error

---
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ThemeChangerScreen extends StatelessWidget {
+
+  static const name = 'theme_changer_screen';
+  
+  const ThemeChangerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme Changer'),
+        actions: [
+          
+        ],
+      ),
+    );
+  }
+}
