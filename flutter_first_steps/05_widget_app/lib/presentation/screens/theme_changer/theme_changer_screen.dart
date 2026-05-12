eeee179de3c37b9a7057f6489113cf70134e7ce8
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 12:45:01 +0200

    añadido el boton de dark mode a la ventana de tema

--- flutter_first_steps/05_widget_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widget_app/presentation/providers/theme_provider.dart';
 
-class ThemeChangerScreen extends StatelessWidget {
+class ThemeChangerScreen extends ConsumerWidget {
 
   static const name = 'theme_changer_screen';
   
   const ThemeChangerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final isDark = ref.watch(isDarkMode);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Theme Changer'),
         actions: [
-          
+          IconButton(
+            onPressed: (){
+              ref.read(isDarkMode.notifier).update((state) => !state);
+            }, 
+            icon: Icon(isDark ? Icons.light_mode_outlined : Icons.dark_mode_outlined),
+          )
         ],
       ),
     );
