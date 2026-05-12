c73b9b63e2f49404968bd378dd5693a5d026a698
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 12:40:34 +0200

    añadida la exportacion

--- flutter_first_steps/05_widget_app/lib/presentation/screens/screens.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/screens.dart
@@ -1,4 +1,7 @@
 
+
+
+export 'package:widget_app/presentation/screens/theme_changer/theme_changer_screen.dart';
 export 'package:widget_app/presentation/screens/counter/counter_screen.dart';
 export 'package:widget_app/presentation/screens/animated/animated_screen.dart';
 export 'package:widget_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
@@ -9,4 +12,3 @@ export 'package:widget_app/presentation/screens/ui_controls/ui_controls_screen.d
 export 'package:widget_app/presentation/screens/bottons/buttons_screen.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
-
