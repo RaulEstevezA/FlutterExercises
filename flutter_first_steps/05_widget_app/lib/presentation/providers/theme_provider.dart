27ecaa3cebef97cb8ee43ce434e3860ef3b3b76e
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 12:53:16 +0200

    Añadido el provider de colores

--- flutter_first_steps/05_widget_app/lib/presentation/providers/theme_provider.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/providers/theme_provider.dart
@@ -1,4 +1,9 @@
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:widget_app/config/theme/app_theme.dart';
 
 final isDarkMode = StateProvider<bool>((ref) => false);
+
+// Listado de colores inmutable
+final colorListProvider = Provider((ref) => colorList);
