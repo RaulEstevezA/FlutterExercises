40aaf9080b862a1d84abc4f043696981ea4d3970
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 11:38:56 +0200

    Añadido boton flotante

--- flutter_first_steps/05_widget_app/lib/presentation/screens/screens.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/screens.dart
@@ -1,4 +1,5 @@
 
+export 'package:widget_app/presentation/screens/counter/counter_screen.dart';
 export 'package:widget_app/presentation/screens/animated/animated_screen.dart';
 export 'package:widget_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
