ccae8af6e018b365d55a5d402537bbc4645f3e38
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 14:08:40 +0200

    modificado la forma en que se ven las imagenes

--- flutter_first_steps/05_widget_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -18,21 +18,22 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Infinite Scroll'),
-      ),
-
-      body: ListView.builder(
-        itemCount: imagesIds.length,
-        itemBuilder: (context, index){
-          return FadeInImage(
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
-            placeholder: const AssetImage('assets/images/jar-loading.gif'), 
-            image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300'),
-          );
-        }
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index){
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/images/jar-loading.gif'), 
+              image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300'),
+            );
+          }
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
