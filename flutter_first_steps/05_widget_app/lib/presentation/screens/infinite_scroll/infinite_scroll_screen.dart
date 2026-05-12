6f631ab96799dcab59ae0fc4d16d532cc6a2225d
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-12 14:03:07 +0200

    añadido fotos que cargan

--- flutter_first_steps/05_widget_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ flutter_first_steps/05_widget_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
 
   static const name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [237,219,218,244,200];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Infinite Scroll'),
+      ),
+
+      body: ListView.builder(
+        itemCount: imagesIds.length,
+        itemBuilder: (context, index){
+          return FadeInImage(
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 300,
+            placeholder: const AssetImage('assets/images/jar-loading.gif'), 
+            image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300'),
+          );
+        }
+      ),
+    );
   }
 }
