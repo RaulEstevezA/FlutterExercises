73fcd3171435098310897acea4ac5bbb658c3281
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-18 22:27:39 +0200

    Agregar manejo de eventos para reiniciar el contador en CounterBloc

--- flutter_first_steps/06_forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ flutter_first_steps/06_forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -8,13 +8,22 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
 
     on<CounterIncresed>( _onCounterIncrease );
+    on<CounterReset>( _onCounterReset );
 
   }
 
   void _onCounterIncrease( CounterIncresed event, Emitter<CounterState> emit) {
     emit(state.copyWith(
         counter: state.counter + event.value,
+        transactionCount: state.transactionCount + 1
+      ));
+  }
+
+  void _onCounterReset( CounterReset event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+        counter: 0,
         transactionCount: state.transactionCount
       ));
   }
+
 }
