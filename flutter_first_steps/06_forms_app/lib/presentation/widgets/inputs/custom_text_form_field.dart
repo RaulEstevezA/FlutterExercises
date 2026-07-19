df660a39354e7aaa2030d817e564814d25fe454d
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-19 14:21:06 +0200

    Agregar lógica de validación y personalización al CustomTextFormField

--- flutter_first_steps/06_forms_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ flutter_first_steps/06_forms_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -5,6 +5,39 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+
+    final colors = Theme.of(context).colorScheme;
+
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    return TextFormField(
+      onChanged: (value) {
+        print('value $value');
+      },
+
+      validator: (value) {
+        if (value == null) return 'Campo requerio';
+        if (value.trim().isEmpty) return 'Campo requerio';
+
+        return null;
+      },
+
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        errorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red)),
+        focusedErrorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red)),
+
+        isDense: true,
+        label: Text('Cualquier cosa'),
+        hintText: 'Este es el hintText',
+        errorText: 'Este es el errorText',
+        focusColor: colors.primary,
+      ),
+    );
+
+
   }
 }
