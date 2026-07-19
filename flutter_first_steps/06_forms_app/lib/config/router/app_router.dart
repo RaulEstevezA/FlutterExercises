ce6e698188e79ab8bdaa7764c6317bb71dbfc4cc
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-19 13:21:04 +0200

    Agregar ruta para la pantalla de registro en el enrutador de la aplicación

--- flutter_first_steps/06_forms_app/lib/config/router/app_router.dart
+++ flutter_first_steps/06_forms_app/lib/config/router/app_router.dart
@@ -18,5 +18,10 @@ final appRouter = GoRouter(
       path: "/counter-bloc",
       builder: (context, state) => const BlockCounterScreen()
     ),
+
+    GoRoute(
+      path: "/new-user",
+      builder: (context, state) => const RegisterScreen()
+    ),
   ]
 );
