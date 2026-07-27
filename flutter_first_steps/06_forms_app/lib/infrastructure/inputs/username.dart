61811690c7e2b70f42e5a3a5de4cd94af44df375
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-27 20:40:25 +0200

    Corregir la validación de entrada en la clase Username para incluir longitud mínima y manejo de espacios en blanco

--- flutter_first_steps/06_forms_app/lib/infrastructure/inputs/username.dart
+++ flutter_first_steps/06_forms_app/lib/infrastructure/inputs/username.dart
@@ -5,15 +5,20 @@ enum UsernameError { empty, lenght }
 
 // Extend FormzInput and provide the input type and error type.
 class Username extends FormzInput<String, UsernameError> {
+
   // Call super.pure to represent an unmodified form input.
   const Username.pure() : super.pure('');
 
   // Call super.dirty to represent a modified form input.
-  const Username.dirty({String value = ''}) : super.dirty(value);
+  const Username.dirty(String value) : super.dirty(value);
 
   // Override validator to handle validating a given input value.
   @override
   UsernameError? validator(String value) {
-    return value.isEmpty ? UsernameError.empty : null;
+    if ( value.trim().isEmpty) return UsernameError.empty;
+    if ( value.length < 6) return UsernameError.lenght;
+
+
+    return null;
   }
 }
