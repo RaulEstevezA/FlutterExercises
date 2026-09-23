432c17009fbdb6d1e238e7e5e90c0158b3643be2
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-23 23:20:22 +0200

    Agregar inicialización de notificaciones de Firebase en la función main

--- flutter_first_steps/07_push_app/lib/main.dart
+++ flutter_first_steps/07_push_app/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:push_app/config/theme/app_theme.dart';
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsBloc.initializeFirebaseNotifications();
 
   runApp(
     MultiBlocProvider(
