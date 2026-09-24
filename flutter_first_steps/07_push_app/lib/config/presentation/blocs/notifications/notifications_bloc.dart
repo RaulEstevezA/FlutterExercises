47febe701e161fd1d85f131d5160297680fc5cf9
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-24 15:22:39 +0200

    Agregar método getMessageById para obtener notificaciones por ID en NotificationsBloc

--- flutter_first_steps/07_push_app/lib/config/presentation/blocs/notifications/notifications_bloc.dart
+++ flutter_first_steps/07_push_app/lib/config/presentation/blocs/notifications/notifications_bloc.dart
@@ -101,7 +101,11 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   }
 
-  
+  PushMessage? getMessageById( String pushMessageId ) {
+    final exist = state.notifications.any((element) => element.messageId == pushMessageId );
+    if ( !exist ) return null;
 
+    return state.notifications.firstWhere((element) => element.messageId == pushMessageId );
+  }
 
 }
