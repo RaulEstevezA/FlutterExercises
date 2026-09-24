b589b4f2bcbd319c370b0e84e20ade28d24c00f0
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-24 20:17:43 +0200

    Eliminar importación innecesaria de app_router en local_notifications.dart

--- flutter_first_steps/07_push_app/lib/config/local_notifications/local_notifications.dart
+++ flutter_first_steps/07_push_app/lib/config/local_notifications/local_notifications.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:push_app/config/router/app_router.dart';
 
 
 Future<void> requestPermissionLocalNotifications() async {
