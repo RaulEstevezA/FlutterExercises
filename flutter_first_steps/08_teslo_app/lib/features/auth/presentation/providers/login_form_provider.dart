93c10a180b012cd02d7f30dd00caf1277cedc985
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-25 22:08:34 +0200

    Agregar validación del formulario en LoginFormNotifier para gestionar cambios en email y contraseña

--- flutter_first_steps/08_teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ flutter_first_steps/08_teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,6 +1,5 @@
 import 'package:teslo_shop/features/shared/shared.dart';
 import 'package:formz/formz.dart';
-import 'package:teslo_shop/features/shared/shared.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // 1 - State del provider
@@ -47,4 +46,52 @@ class LoginFormState {
   }
 }
 
+// 2 - Como implementamos un notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
 
+  final Function(String, String) loginUserCallback;
+
+  LoginFormNotifier({
+    required this.loginUserCallback,
+  }): super( LoginFormState() );
+  
+  onEmailChange( String value ) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([ newEmail, state.password ])
+    );
+  }
+
+  onPasswordChanged( String value ) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([ newPassword, state.email ])
+    );
+  }
+
+  onFormSubmit() async {
+    _touchEveryField();
+
+    if ( !state.isValid ) return;
+
+    await loginUserCallback( state.email.value, state.password.value );
+
+  }
+
+  _touchEveryField() {
+
+    final email    = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([ email, password ])
+    );
+
+  }
+
+}
