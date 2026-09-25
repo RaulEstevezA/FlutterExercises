68fc6b874518b84f74531ec9bc11c213e810cba2
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-25 22:06:48 +0200

    Agregar estado y métodos al LoginFormState para gestionar el formulario de inicio de sesión

--- flutter_first_steps/08_teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ flutter_first_steps/08_teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,5 +1,9 @@
 import 'package:teslo_shop/features/shared/shared.dart';
+import 'package:formz/formz.dart';
+import 'package:teslo_shop/features/shared/shared.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// 1 - State del provider
 class LoginFormState {
 
   final bool isPosting;
@@ -8,13 +12,39 @@ class LoginFormState {
   final Email email;
   final Password password;
 
-  LoginFormState(
-    {this.isPosting = false,
+  LoginFormState({
+    this.isPosting = false,
     this.isFormPosted = false,
     this.isValid = false,
     this.email = const Email.pure(),
-    this.password = const Password.pure(),
+    this.password = const Password.pure()
   });
 
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) => LoginFormState(
+    isPosting: isPosting ?? this.isPosting,
+    isFormPosted: isFormPosted ?? this.isFormPosted,
+    isValid: isValid ?? this.isValid,
+    email: email ?? this.email,
+    password: password ?? this.password,
+  );
 
+  @override
+  String toString() {
+    return '''
+  LoginFormState:
+    isPosting: $isPosting
+    isFormPosted: $isFormPosted
+    isValid: $isValid
+    email: $email
+    password: $password
+''';
+  }
 }
+
+
