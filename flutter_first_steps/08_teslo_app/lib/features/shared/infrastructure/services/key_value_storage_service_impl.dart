68996ed2b0659d70b83688a19a4b89e445ef4242
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-09-26 20:43:51 +0200

    Agregar implementación de KeyValueStorageService y su clase concreta para gestionar almacenamiento de clave-valor

--- flutter_first_steps/08_teslo_app/lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
+++ flutter_first_steps/08_teslo_app/lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -1,5 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
-import 'key_value_storage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
 
 
 class KeyValueStorageServiceImpl extends KeyValueStorageService {
